e0f950d0bcde442ed28520b76e20008bb7c496e9
Sam Bezzant <sambezzant@Sams-MBP.home>
2022-03-25 13:07:25 -0700

    server.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// start your server here
+const server = require('./api/server');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+	console.log(`<===|| Server listening on port ${PORT} ||===>`);
+});
