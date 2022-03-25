aee6dce5b895a2fd1932c1a11fda8164c5dcffaf
Sam Bezzant <sambezzant@Sams-MBP.home>
2022-03-25 15:42:16 -0700

    tasks router, model, server

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,28 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+const find = async () => {
+	let result = await db('tasks as t')
+		.leftJoin('projects as p', 't.project_id', 'p.project_id')
+		.select(
+			't.task_completed',
+			't.task_notes',
+			't.task_description',
+			'p.project_name',
+			'p.project_description'
+		);
+
+	return result;
+};
+
+const findById = async (id) => {
+	let result = await db('projects as p').where('p.project_id', id);
+	return result;
+};
+
+const insert = async (task) => {
+	let result = await db('tasks as t').insert(task);
+	let findNewTask = await db('tasks as t').where('task_id', result);
+	return findNewTask;
+};
+
+module.exports = { find, findById, insert };
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,41 @@
-// build your `/api/tasks` router here
+const express = require('express');
+const tasksRouter = express.Router();
+const Tasks = require('./model');
+
+tasksRouter.get('/', (req, res) => {
+	Tasks.find().then((tasks) => {
+		let newArray = [];
+		tasks.map((task) => {
+			newArray.push({ ...task, task_completed: Boolean(task.task_completed) });
+		});
+
+		res.status(200).json(newArray);
+	});
+});
+
+tasksRouter.post('/', (req, res) => {
+	const { task_description, project_id } = req.body;
+	if (!task_description || !project_id) {
+		res
+			.status(400)
+			.json({ message: 'Task Description and Project ID are required!' });
+	} else {
+		Tasks.findById(project_id).then((project) => {
+			if (project.length > 0) {
+				Tasks.insert(req.body).then((task) => {
+					// let obj = {
+					// 	task_id: task.task_id,
+					// 	task_description: task.task_description,
+					// 	task_notes: task.task_notes,
+					// 	task_completed: Boolean(task.task_completed),
+					// };
+					res.json(task);
+				});
+			} else {
+				res.status(404).json("Project doesn't exist!");
+			}
+		});
+	}
+});
+
+module.exports = tasksRouter;
