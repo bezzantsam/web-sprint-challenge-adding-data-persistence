316d0b631951f14c407ced082ce73410ebf19b55
Sam Bezzant <sambezzant@Sams-MBP.home>
2022-03-25 13:55:45 -0700

    project router and model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,21 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const find = () => {
+	return db('projects');
+};
+
+const create = async (project) => {
+	const result = await db('projects').insert(project);
+	return findById(result);
+};
+
+const findById = (id) => {
+	return find().where({ project_id: id }).first();
+};
+
+module.exports = {
+	find,
+	create,
+	findById,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,40 @@
 // build your `/api/projects` router here
+const express = require('express');
+const projectsRouter = express.Router();
+const Projects = require('./model');
+
+projectsRouter.get('/', (req, res) => {
+	Projects.find().then((projects) => {
+		let newArray = [];
+		projects.map((project) => {
+			newArray.push({
+				...project,
+				project_completed: Boolean(project.project_completed),
+			});
+		});
+		res.status(200).json(newArray);
+	});
+});
+
+projectsRouter.post('/', (req, res) => {
+	const { project_name, project_description, project_completed } = req.body;
+	if (project_name !== undefined) {
+		let obj = {
+			project_name,
+			project_description,
+			project_completed,
+		};
+
+		if (!project_completed) {
+			obj.project_completed = false;
+		}
+
+		Projects.create(obj).then((project) => {
+			res.status(201).json(project);
+		});
+	} else {
+		res.status(400).json('Bad Request!');
+	}
+});
+
+module.exports = projectsRouter;
